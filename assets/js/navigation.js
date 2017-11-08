869a4d290fc05e3be8f094297ee8ccb62861b78d
sriharsha <harsha.chayanam@gmail.com>
2017-11-08 20:15:16 +0530

    removed unnecessary code

---
+++ assets/js/navigation.js
@@ -0,0 +1,13 @@
+$('.get_dashboard').click(function() {
+    window.location.assign("pages/home.html");
+});
+$('.get_users').click(function() {
+	console.log("hola datevid");
+    window.location.assign("pages/user/users.html");
+});
+$('.get_drawings').click(function() {
+    window.location.assign("pages/drawing/drawings.html");
+});
+$('.create_drawing').click(function() {
+    window.location.assign("pages/drawing/newdrawing.html");
+});
